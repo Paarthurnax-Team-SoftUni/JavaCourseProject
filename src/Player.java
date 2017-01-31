a3eae14da265ebbb33033281a09c9627e1f5750d
totopopov <totopopov@gmail.com>
2017-01-31 13:20:43 +0200

    Class player created with basic props

---
+++ src/Player.java
@@ -0,0 +1,61 @@
+
+public class Player {
+
+
+   private String name;
+   private Long highScore;
+   private Double money;
+   private Long points;
+   private Long experience;
+   private int healthPoingts;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getHighScore() {
+        return highScore;
+    }
+
+    public void setHighScore(Long highScore) {
+        this.highScore = highScore;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    public Long getPoints() {
+        return points;
+    }
+
+    public void setPoints(Long points) {
+        this.points = points;
+    }
+
+    public Long getExperience() {
+        return experience;
+    }
+
+    public void setExperience(Long experience) {
+        this.experience = experience;
+    }
+
+    public int getHealthPoingts() {
+        return healthPoingts;
+    }
+
+    public void setHealthPoingts(int healthPoingts) {
+        this.healthPoingts = healthPoingts;
+    }
+
+
+}
