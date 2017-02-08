e3f0b7c4f9535e103c6870d97f1265a74a4afc65
ChristianGeorgiev <kristian.v.georgiev@gmail.com>
2017-02-08 18:04:14 +0200

    Generate random obstacle Have to make it more than 1

--- src/GameLogic/Game.java
+++ src/GameLogic/Game.java
@@ -18,8 +18,10 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 import main.Main;
 
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Random;
 
 public class Game {
     private static AnchorPane root = Main.windowPane;
@@ -48,6 +50,10 @@ public class Game {
         //playerCar.setImage("/resources/images/player_car2.png");  depending on level?
         playerCar.setPosition(200, 430);
 
+
+        Sprite testObstacle = generateObstacle();
+
+
         Timeline gameLoop = new Timeline();
         gameLoop.setCycleCount(Timeline.INDEFINITE);
         KeyFrame kf = new KeyFrame(
@@ -62,6 +68,7 @@ public class Game {
                      //   System.out.println(y);
                     }
                     //520 : 8
+
 
                     playerCar.setVelocity(0, 0);
                     if (input.contains("LEFT")) {
@@ -79,11 +86,33 @@ public class Game {
                     gc.clearRect(0, 0, 500, 600);
                     gc.drawImage(background, 0, y);
                     gc.drawImage(background, 0, y-600);
+                    testObstacle.setVelocity(0, y/47);
+                    testObstacle.render(gc);
+                    testObstacle.update();
                     playerCar.render(gc);
+
+                    if (testObstacle.getBoundary().intersects(playerCar.getBoundary())){
+                        gameLoop.stop();
+                    }
                 }
             });
 
         gameLoop.getKeyFrames().add(kf);
         gameLoop.play();
+
+
+    }
+    private static Sprite generateObstacle (){
+        Random obstacleX = new Random();
+        Random obstacleY = new Random();
+        Random obstaclePic = new Random();
+        long numb = System.currentTimeMillis()%2;
+
+        String sd = "/resources/images/obstacle" + (numb+1) + ".png";
+        Sprite testObstacle = new Sprite();
+        testObstacle.setImage(sd);
+        testObstacle.setPosition(obstacleX.nextInt(400), obstacleY.nextInt(200));
+
+        return testObstacle;
     }
 }
