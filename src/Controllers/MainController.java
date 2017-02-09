ca67eb1f515122dcea1a55facda4e75e688470ca
mitkovaelena <mitkova_elena@yahoo.com>
2017-02-09 11:42:29 +0200

    bugs fixed

--- src/Controllers/MainController.java
+++ src/Controllers/MainController.java
@@ -1,5 +1,8 @@
 package Controllers;
 
+
+import DataHandler.Player;
+import DataHandler.PlayerData;
 import MapHandlers.Track;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,6 +16,7 @@ import javafx.scene.control.Dialog;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -24,26 +28,37 @@ import main.Main;
 
 import java.awt.*;
 import java.io.IOException;
+import java.util.Optional;
 
 public class MainController {
 
     @FXML
     public AnchorPane loginPage;
     public static AnchorPane homePage;
-    @FXML
+    public AnchorPane chooseCarPage;
     public Button loginBtn;
+    @FXML
     public TextField playerName;
+    @FXML
     public Button showScoresBtn;
+    @FXML
     public Button closeBtn;
+    @FXML
     public Button startBtn;
+    @FXML
     public Button chooseCarBtn;
+    @FXML
     public Rectangle backgroundBox;
+    @FXML
+    public Button returnBtn;
 
 
     @FXML
     public void showHighScores() {
         showHighScoresDialog();
     }
+
+    public static Player player;
 
     public static void MainController(){
 
@@ -74,10 +89,8 @@ public class MainController {
         root.getChildren().add(gridpane);
         stage.setScene(new Scene(root, 800, 600));
         stage.show();
-
-
     }
-
+    
     private void showHighScoresDialog() {
         Dialog<ButtonType> dialog = new Dialog<>();
         dialog.setTitle("Best Slav Ranking");
@@ -98,7 +111,7 @@ public class MainController {
     @FXML
     public void startNewGame() {
 
-        Track.createBackground();
+        Track.initializeLevel(1);
 
         showScoresBtn.setVisible(false);
         startBtn.setVisible(false);
@@ -117,7 +130,41 @@ public class MainController {
         stage.close();
     }
 
+    @FXML
     public void showStartPage() throws IOException {
+        String name = playerName.getText().trim();
+        if ("".equals(name)) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("You have to fill in the username field!");
+            alert.setContentText("Please click on OK to retry!");
+            alert.setHeaderText("You have to fill in the username field!");
+            Optional<ButtonType> result = alert.showAndWait();
+        } else if (!PlayerData.getInstance().checkForPlayer(name)) {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Login with this username");
+            alert.setHeaderText("Login as user: " + name);
+            alert.setContentText("Are you sure? Press OK to continue, or Cancel to abort.");
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.isPresent() && (result.get() == ButtonType.OK)) {
+                player = PlayerData.getInstance().returnPlayer(name);
+                renderStartMenu();
+            }
+        } else {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Create new user");
+            alert.setHeaderText("Create new user: " + name);
+            alert.setContentText("Are you sure? Press OK to continue, or Cancel to abort.");
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.isPresent() && (result.get() == ButtonType.OK)) {
+                player = new Player(name, 0L, 0.0, 0L, 0L, 100);
+                /* remove the comments to save every newly created player*/
+//                PlayerData.getInstance().addPlayer(player);
+//                PlayerData.getInstance().storePlayersData();
+                renderStartMenu();
+            }
+        }
+    }
+    private void renderStartMenu() throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("../views/startPage.fxml"));
         AnchorPane windowPane = (AnchorPane) root.lookup("#homePage");
         Main.windowPane=windowPane;
@@ -126,5 +173,6 @@ public class MainController {
         stage.setScene(new Scene(root, 800, 600));
         stage.show();
     }
+
 
 }
