bdd5e50d946e4c721ea69e28696386a8aedfeb95
inaabadjieva <ina_abadjieva@yahoo.com>
2017-03-03 19:22:57 +0200

    Stage Interface for changing scenes added

---
+++ src/stageHandler/StageManager.java
@@ -0,0 +1,10 @@
+package stageHandler;
+
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public interface StageManager {
+
+    FXMLLoader loadSceneToStage(Stage currentStage, String fxmlPath, Modality modality);
+}
